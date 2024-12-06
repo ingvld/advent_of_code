b59ca56eaefdc75a18115ea78829bdc971ef84e4
Ingvild Utvik <ingvildgurine@gmail.com>
2024-12-06 22:37:09 +0100

    finished day 3

--- 2024/src/days/d3.rs
+++ 2024/src/days/d3.rs
@@ -13,11 +13,24 @@ pub fn p1(input : String) -> i32 {
 }
 
 pub fn p2(input : String) -> i32 {
-   let re = Regex::new(r"mul\(d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
-   
-   for m in re.find_iter(&input) {
-       dbg!(m);
-   }
+   let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
+   let mut mul = true;
 
-   3
+   re.find_iter(&input)
+       .fold(0, |acc, m| acc + {
+           match m.as_str() {
+               "do()" => {mul = true; 0},
+               "don't()" => {mul = false; 0}
+               x => {
+                   if !mul {0}
+                   else {
+                       x.split(&['(',',',')'])
+                           .skip(1)
+                           .take(2)
+                           .map(|n| n.parse::<i32>().unwrap())
+                           .product()
+                   }
+               }
+           }
+       })
 }
