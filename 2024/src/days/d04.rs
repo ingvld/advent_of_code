f8978663dfa0cc3d468b7fbfa34df5b3281cde9b
Ingvild Utvik <ingvildgurine@gmail.com>
2024-12-08 22:54:06 +0100

    Initial setup for remaining days

--- 2024/src/days/d04.rs
+++ 2024/src/days/d04.rs
@@ -17,7 +17,7 @@ pub fn p1(input : String) -> i32 {
     1
 }
 
-pub fn p2(input : String) -> i32 {
+pub fn p2(_input : String) -> i32 {
     2
 }
 
---
+++ 2024/src/days/d20.rs
@@ -0,0 +1,7 @@
+pub fn p1(_input : String) -> i32 {
+    1
+}
+
+pub fn p2(_input : String) -> i32 {
+    2
+}
