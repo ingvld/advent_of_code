db08855246d1687a38dbaf1a7ed2b6070c2dda24
Ingvild Utvik <ingvildgurine@gmail.com>
2024-12-19 14:33:26 +0100

    Changed test-files to have optional part specification

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,9 +1,9 @@
-use std::{fs,env};
 use aoc_2024::run_day;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() < 3 {
         eprintln!("Too few arguments. Usage: cargo run day part (optionally -t for test-data)");
         return;
@@ -11,17 +11,24 @@ fn main() {
 
     let day: usize = args[1].parse().expect("Day must be a number");
     let part: usize = args[2].parse().expect("Part must be a number");
-    
-    let file_path = match args.get(3) {
-        Some(t) if t == "-t" => format!("../inputs/{day}-test-{part}"),
-        None => format!("../inputs/{day}-input"),
-        _ => { eprintln!("Only valid flag is -t for running against test data. by default input data is use"); return;}
-      };
-    
-    let input = fs::read_to_string(file_path);
+
+    let input = match args.get(3) {
+        Some(t) if t == "-t" => {
+            let solo = fs::read_to_string(format!("../inputs/{day}-test-{part}"));
+            match solo {
+                Ok(_) => solo,
+                Err(_) => fs::read_to_string(format!("../inputs/{day}-test")),
+            }
+        }
+        None => fs::read_to_string(format!("../inputs/{day}-input")),
+        _ => {
+            eprintln!("Only valid flag is -t for running against test data. by default full input data is used");
+            return;
+        }
+    };
 
     match input {
-        Ok(input) => run_day(day,part,input),
+        Ok(input) => run_day(day, part, input),
         Err(e) => panic!("{e}"),
     };
 }
