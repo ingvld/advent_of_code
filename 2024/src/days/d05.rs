c3068809d5106be4bcc2d9c45de539e9877d0d76
Ingvild Utvik <ingvildgurine@gmail.com>
2025-06-22 12:13:03 +0200

    24 day 5

--- 2024/src/days/d05.rs
+++ 2024/src/days/d05.rs
@@ -1,7 +1,40 @@
-pub fn p1(_input : String) -> i32 {
-    1
+use std::collections::{HashMap, HashSet};
+
+pub fn p1(input: String) -> i32 {
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+
+    let mut rulemap: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for rule in rules.lines() {
+        let (v, k) = rule.split_once("|").unwrap();
+        rulemap.entry(k).or_insert(HashSet::new()).insert(v);
+    }
+
+    let mut sum = 0;
+
+    for update in updates.lines().map(|l| l.split(',').collect::<Vec<&str>>()) {
+        let mid = update[update.len() / 2].parse::<i32>().unwrap();
+        let mut incl = true;
+
+        let mut illegals: HashSet<&str> = HashSet::new();
+
+        for x in update {
+            if illegals.contains(x) {
+                incl = false;
+            }
+            if rulemap.contains_key(x) {
+                illegals.extend(rulemap.get(x).unwrap());
+            }
+        }
+
+        if incl {
+            sum += mid;
+        }
+    }
+
+    sum
 }
 
-pub fn p2(_input : String) -> i32 {
+pub fn p2(_input: String) -> i32 {
     2
 }
