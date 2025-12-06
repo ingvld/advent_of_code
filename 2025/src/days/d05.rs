29937c52f0ed4a8ecc0171fa6986171e15738124
Ingvild Utvik <ingvildgurine@gmail.com>
2025-12-06 11:20:12 +0100

    Revert "2025 setup"
    This reverts commit b0bc8ebf5651d41ef4820366f372a1530e7b871a.

--- 2025/src/days/d05.rs
+++
@@ -1,7 +0,0 @@
-pub fn p1(_input : String) -> i32 {
-    1
-}
-
-pub fn p2(_input : String) -> i32 {
-    2
-}
--- 2025/target/debug/deps/aoc_2025-1110c865ada9bbab.d
+++
@@ -1,5 +0,0 @@
-/home/ingvld/advent_of_code/2025/target/debug/deps/aoc_2025-1110c865ada9bbab: src/main.rs
-
-/home/ingvld/advent_of_code/2025/target/debug/deps/aoc_2025-1110c865ada9bbab.d: src/main.rs
-
-src/main.rs:
